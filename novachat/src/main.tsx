cff21582b8e322accf0aef4f2e4c61302b83c9dc
unknown <kalidhaskd14@gmail.com>
2026-08-07 17:43:00 +0530

    Fix react-router build

--- novachat/src/main.tsx
+++ novachat/src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from 'react-router';
 
 import App from './App';
 
 import { ThemeProvider } from './context/ThemeContext';
 import { WallpaperProvider } from './context/WallpaperContext';
 import { AuthProvider } from './context/AuthContext';
+import { KeyboardProvider } from './context/KeyboardContext';
+import { UnreadProvider } from './context/UnreadContext';
 
 import ErrorBoundary from './components/ErrorBoundary';
 
 import './index.css';
-
 
 const root = document.getElementById('root');
 
@@ -19,33 +20,22 @@ if (!root) {
   throw new Error('Root element not found');
 }
 
-
 createRoot(root).render(
-
   <StrictMode>
-
     <ErrorBoundary>
-
       <BrowserRouter>
-
         <ThemeProvider>
-
           <WallpaperProvider>
-
             <AuthProvider>
-
-              <App />
-
+              <UnreadProvider>
+                <KeyboardProvider>
+                  <App />
+                </KeyboardProvider>
+              </UnreadProvider>
             </AuthProvider>
-
           </WallpaperProvider>
-
         </ThemeProvider>
-
       </BrowserRouter>
-
     </ErrorBoundary>
-
   </StrictMode>
-
 );
